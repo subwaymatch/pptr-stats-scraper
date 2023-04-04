64c4bc715e2cb68b1c5159210b8c7269239a7124
Ye Joo Park <subwaymatch@gmail.com>
2023-04-04 02:11:44 -0500

    Create mapping of NCAA sports ids to diferent platforms

---
+++ src/constants/sports.ts
@@ -0,0 +1,152 @@
+import { WebsitePlatform } from "@prisma/client";
+
+export const sportsIdMap = {
+  "cross-country-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mens-cross-country",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "cross-country-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-cross-country",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  fieldhockey: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "field-hockey",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  football: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "football",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "soccer-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mens-soccer",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "soccer-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-golf",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "volleyball-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-volleyball",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "waterpolo-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mens-water-polo",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "basketball-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-basketball",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  bowling: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-bowling",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  fencing: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "gymnastics-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mens-gymnastics",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "gymnastics-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-gymnastics",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "icehockey-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mens-ice-hockey",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "icehockey-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  rifle: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  skiing: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "swimming-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "swimming-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "trackfield-indoor-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "trackfield-indoor-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  wrestling: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "wrestling",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "beach-volleyball": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "golf-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mens-golf",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "golf-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-golf",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "lacrosse-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mlax",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "lacrosse-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-lacrosse",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  rowing: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-rowing",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  softball: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "softball",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "tennis-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mens-tennis",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "tennis-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-tennis",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "trackfield-outdoor-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "trackfield-outdoor-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "volleyball-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mens-volleyball",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "waterpolo-women": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "womens-water-polo",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  "basketball-men": {
+    [WebsitePlatform.SIDEARM_SPORTS]: "mens-basketball",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+  baseball: {
+    [WebsitePlatform.SIDEARM_SPORTS]: "baseball",
+    [WebsitePlatform.PRESTO_SPORTS]: "",
+  },
+};
